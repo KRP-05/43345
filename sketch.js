9169a72ef8463c641e0e01cbdb1b16a4a8b0cdfa
KRP-05 <74408341+KRP-05@users.noreply.github.com>
2022-01-10 23:17:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var gamestate=0
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(750,500);
+  
+
+	playbutton=createSprite(750/2,200,150,45)
+  playbutton.visible=false
+
+  soundoff=createSprite(700,50,30,30)
+
+
+
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(200);
+
+  if(gamestate===0){
+    playbutton.visible=true
+
+    if(mousePressedOver(playbutton)){
+      gamestate=1
+    }
+    
+  }
+  if(gamestate===1){
+    bb=createSprite(100,100,100,100)
+    bb.shapeColor='red'
+  }
+  
+  drawSprites();
+ 
+}
+
+
+
